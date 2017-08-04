bd00faa2bedc04ec28cc38b7a6dbcfe820b5ed51
Marcel Killin <killinmarcelsp@gmail.com>
2017-08-04 16:57:12 -0400

    only open index.html in browser

--- pair_project/src/gameboard.js
+++ pair_project/src/gameboard.js
@@ -5,14 +5,11 @@ import './App.css';
 class Gameboard extends Component {
     constructor() {
         super();
+        document.createElement("canvas")
         this.startGame = this.startGame.bind(this);
         this.component2 = this.component2.bind(this);
         this.updateGameArea = this.updateGameArea.bind(this);
-
-        
-
-
-        var myGameArea = {
+        this.state = {
             canvas: document.createElement("canvas"),
             start: function () {
                 this.canvas.width = 480;
@@ -31,19 +28,36 @@ class Gameboard extends Component {
 
 
 
+        // var myGameArea = {
+        //     canvas: document.createElement("canvas"),
+        //     start: function () {
+        //         this.canvas.width = 480;
+        //         this.canvas.height = 270;
+        //         this.context = this.canvas.getContext("2d");
+        //         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        //         this.interval = setInterval(this.updateGameArea, 20);
+        //     },
+        //     stop: function () {
+        //         clearInterval(this.interval);
+        //     },
+        //     clear: function () {
+        //         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        //     }
+        // }
+
+
+
 
 
 
     }
     componentDidMount() {
-        this.startGame
+        // console.log("did mount");
+        // this.updateGameArea();
     }
     startGame() {
-        var myGamePiece;
-        var myGamePiece2;
-        myGamePiece = new this.component2(30, 30, "red", 100, 300);
-        myGamePiece2 = new this.component2(30, 30, "green", 300, 300);
-        this.myGameArea.start();
+
+        this.state.start();
     }
     component2(width, height, color, x, y, type) {
         this.type = type;
@@ -53,10 +67,10 @@ class Gameboard extends Component {
         this.y = y;
         this.speedX = 0;
         this.speedY = 0;
-        this.gravity = - .1;
+        this.gravity = - 0.1;
         this.gravitySpeed = 0;
         this.update = function () {
-            let ctx = myGameArea.context;
+            let ctx = this.myGameArea.context;
             ctx.fillStyle = color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
@@ -67,16 +81,23 @@ class Gameboard extends Component {
         }
     }
     updateGameArea() {
-        myGameArea.clear();
+        var myGamePiece;
+        var myGamePiece2;
+        myGamePiece = new this.component2(30, 30, "red", 100, 300);
+        myGamePiece2 = new this.component2(30, 30, "green", 300, 300);
+        this.state.clear();
         myGamePiece.newPos();
         myGamePiece.update();
         myGamePiece2.newPos();
         myGamePiece2.update();
     }
     render() {
+       
         return (
             <div className="Gameboard">
-
+                <canvas>
+                    {this.component2(30, 30, "red", 100, 300)}
+                    </canvas>
 
             </div>
         );
